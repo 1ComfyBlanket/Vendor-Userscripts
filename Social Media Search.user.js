1ddb7a9174282dd88e6a6aaebbef57b16fca7ee8
Wilbert Siojo <wilbert.siojo@gmail.com>
2021-06-17 12:37:25 -0700

    Add files via upload
    Social Media Search v1.1.3
    * Again fixed a bug related to Search buttons not clearing properly on task switching.

--- Social Media Search.user.js
+++ Social Media Search.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Social Media Search
 // @namespace    http://tampermonkey.net/
-// @version      1.1.2
+// @version      1.1.3
 // @update       https://github.com/1ComfyBlanket/Covey-Userscripts/raw/main/Social%20Media%20Search.user.js
 // @description  For searching email handles on various social media sites in a single click.
 // @author       Wilbert Siojo
@@ -70,12 +70,14 @@ function searchEmail() {
 
 let profileName
 let emailButtonArray = [];
-let emailSectionArray = [];
+let emailHandle
 function createSocialMediaButton() {
     // For checking against the currently selected profile
     let profileNameArray = document.getElementsByClassName('text-xl font-normal text-gray-900 truncate -mb-1');
-    if (profileNameArray.length === 0) { return };
-    if (profileName !== profileNameArray[0].innerHTML.split('<')[0].toLowerCase()) {
+    let emailSectionArray = document.getElementsByClassName('text-sm text-gray-900 font-semibold');
+    if (profileNameArray.length === 0 || emailSectionArray.length === 0) { return };
+    // if (profileName !== profileNameArray[0].innerHTML.split('<')[0].toLowerCase()) {
+    if (emailHandle !== emailSectionArray[0].innerText.split('@')[0]) {
         for (let i = 0; i < emailButtonArray.length; i++) {
             // Destroy all search buttons when profile changes
             emailButtonArray[i].remove();
@@ -83,9 +85,10 @@ function createSocialMediaButton() {
         for (let i = 0; i < emailSectionArray.length; i++) {
             emailSectionArray[i].removeAttribute('id')
         }
-            emailButtonArray = [];
+            emailButtonArray = []
     };
     profileName = profileNameArray[0].innerHTML.split('<')[0].toLowerCase();
+    emailHandle = emailSectionArray[0].innerText.split('@')[0];
 
     // Individual email sections are put into an array
     emailSectionArray = document.getElementsByClassName('text-sm text-gray-900 font-semibold');
@@ -97,6 +100,8 @@ function createSocialMediaButton() {
         emailButton.setAttribute('id', idString);
 
         // Check if user is in the "Email finder" tab
+        let matchConfidence = 0
+        let abbreviatedNameArray = []
         let emailFinderTab = document.getElementsByClassName('-mb-px flex space-x-8')[0].children[0].className;
         if(emailFinderTab === 'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm border-gray-800 text-gray-900 ') {
             // Filter out emails if they are abbreviated and 0% match confidence
@@ -104,14 +109,12 @@ function createSocialMediaButton() {
             // profileName = profileNameArray[0].innerHTML.split('<')[0].toLowerCase();
             let profileNameSplit = profileName.split(' ');                                                        // [john, smith]
             // Generate abbreviated versions of this name
-            let abbreviatedNameArray = [];
             abbreviatedNameArray.push(`${profileNameSplit[0].charAt(0)}${profileNameSplit[1]}`);                  // jsmith
             abbreviatedNameArray.push(`${profileNameSplit[0].charAt(0)}.${profileNameSplit[1]}`);                 // j.smith
             abbreviatedNameArray.push(`${profileNameSplit[1]}${profileNameSplit[0].charAt(0)}`);                  // smithj
             abbreviatedNameArray.push(`${profileNameSplit[1]}.${profileNameSplit[0].charAt(0)}`);                 // smith.j
             // Retrieve the match confidence value and email handle
-            let matchConfidence = 0;
-            let emailHandle = emailSectionArray[i].innerText.split('@')[0];
+            emailHandle = emailSectionArray[i].innerText.split('@')[0];
             let divEmailSections = emailSectionArray[i].parentElement.parentElement.nextSibling.children;
             // If profile has an avatar search one child further
             let divEmailSectionsNum;
@@ -132,10 +135,10 @@ function createSocialMediaButton() {
         socialMediaButton.addEventListener('click', searchEmail, false);
         socialMediaButton.appendChild(document.createTextNode('Search'));
         emailButton.parentNode.insertBefore(socialMediaButton, emailButton.nextSibling.nextSibling);
-        //Set className for CSS
-        socialMediaButton.className = 'searchButton';
         // Add to  button array for deletion when profile changes
         emailButtonArray.push(socialMediaButton);
+        //Set className for CSS
+        socialMediaButton.className = 'searchButton';
     };
 }
 
